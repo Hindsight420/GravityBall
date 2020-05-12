0e4b346c03831b590e13aff6dfa925b1dbb61629
Milan <milan.andries@live.be>
2020-05-12 13:18:06 +0200

    Added game project

---
+++ Project/Assets/Scripts/GravityCircle.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityCircle : MonoBehaviour
+{
+    Sprite originalSprite;
+    Sprite activatedSprite;
+    SpriteRenderer rend;
+    Rigidbody2D playerRB;
+    Vector2 coords;
+    bool isActivated;
+    [SerializeField]
+    const float pull = 100;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = GetComponent<SpriteRenderer>();
+        originalSprite = rend.sprite;
+        activatedSprite = Resources.Load<Sprite>("openCircles_03");
+
+        playerRB = GameObject.Find("Player").GetComponent<Rigidbody2D>();
+        coords = GetComponent<Transform>().position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        if (isActivated)
+        {
+            Vector2 direction = (Vector2)transform.position - playerRB.position;
+            playerRB.AddForce(direction * pull * Time.deltaTime);
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        rend.sprite = activatedSprite;
+        isActivated = true;
+    }
+
+    private void OnMouseUp()
+    {
+        rend.sprite = originalSprite;
+        isActivated = false;
+    }
+}
---
+++ Project/Assets/Scripts/Reset.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Events;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Reset : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Start()
+    {
+        Time.timeScale = 1;
+    }
+    void Awake()
+    {
+
+    }
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        Button btn = GetComponent<Button>();
+        btn.onClick.AddListener(Click);
+    }
+
+    // Update is called once per frame
+    void Click()
+    {
+        int Index = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(Index);
+        Debug.Log("Hello world!");
+    }
+}
