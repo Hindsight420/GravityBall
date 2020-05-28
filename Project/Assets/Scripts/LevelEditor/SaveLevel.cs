0a67d447850ac6a0640b03560260c55a53c4540e
Dustycube <65244028+Dustycube@users.noreply.github.com>
2020-05-28 23:33:24 +0200

    Added multivalue dictionary to SaveLevel script

--- Project/Assets/Scripts/LevelEditor/SaveLevel.cs
+++ Project/Assets/Scripts/LevelEditor/SaveLevel.cs
@@ -1,27 +1,71 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.UI;
+using Newtonsoft.Json;
 
 public class SaveLevel : MonoBehaviour
 {
-    public List<GameObject> placedObjects;
+    public List<GameObject> placedObjects; // Moet later aangepast worden, is een lijst van objecten, geen prefabs
+    public Button lE_SaveLevelButton;
+    /* public string prefabName;             //Deze twee staan hier voor placeholder in het SaveLevel object
+    public Vector2 objPos;                  */
 
     void Start()
     {
-        print("sup");
+       lE_SaveLevelButton.onClick.AddListener(SaveAsJson);
     }
 
+    /*
+    // Jelle's multivalue dictionary (part 1)
+    public SaveLevel(string prefabNameData, Vector2 objPosData)                  //Nieuw soort object "SaveLevel" met inhoud prefabName en objPos
+    {
+        prefabNameData = prefabName;
+        objPosData = objPos;
+    }                                                                                   */
     public void SaveAsJson()
     {
-        Dictionary<string, Vector2> objects = new Dictionary<string, Vector2>();
-        foreach(GameObject obj in placedObjects)
+        //Zoekt alle GameObjects in de scene met script DraggableElement en plaatst ze in placedObjects
+        foreach (GameObject gameObject in FindObjectsOfType<GameObject>())                      
         {
-            string prefabName = obj.GetComponent<DraggableElement>().linkedPrefab.name;
-            Vector2 objPos = obj.transform.position;
-
-            objects.Add(prefabName, objPos);
+            if (gameObject.GetComponent<DraggableElement>() != null)                            
+            {
+                  placedObjects.Add(gameObject);               
+            }
         }
 
-        string json = JsonUtility.ToJson(objects);
-        print(json);
+
+        //Maakt dictionary met key prefabName en value objPos
+        Dictionary<string, Vector2> objects = new Dictionary<string, Vector2>();     // Maakt dictionary "objects" met namen van de gelinkte prefabs + posities
+         foreach(GameObject obj in placedObjects)
+         {
+             string prefabName = obj.GetComponent<DraggableElement>().linkedPrefab.name;
+             Vector2 objPos = obj.transform.position;
+
+             objects.Add(prefabName, objPos);
+         }
+
+        /*
+        //Jelle's multivalue dictionary (part 2)
+
+        Dictionary<string, SaveLevel> objects = new Dictionary<string, SaveLevel>();
+        foreach(GameObject obj in placedObjects)
+        {
+            string instanceName = obj.name;
+            prefabName = obj.GetComponent<DraggableElement>().linkedPrefab.name;
+            objPos = obj.transform.position;
+            SaveLevel instancePrefabPosition = obj.AddComponent<SaveLevel>();
+
+            objects.Add(instanceName, instancePrefabPosition);
+            Debug.Log(instanceName + " with prefab " + prefabName + " and position " + objPos + " was added to the dictionary.");
+        }
+
+
+        Debug.Log(bjects)
+                                                                                        */
+         
+       string json = JsonConvert.SerializeObject(objects);     // Maakt een JSON van  dictionary "objects"
+       print(json);
     }
 }
+
