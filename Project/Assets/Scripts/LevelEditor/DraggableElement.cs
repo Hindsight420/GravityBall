df281be8c364fbf4be955164077ddcf94c206748
Dustycube <65244028+Dustycube@users.noreply.github.com>
2020-06-30 18:52:35 +0200

    Rewrote LE_ElementButton and DraggableElement to use the EventSystem

--- Project/Assets/Scripts/LevelEditor/DraggableElement.cs
+++ Project/Assets/Scripts/LevelEditor/DraggableElement.cs
@@ -1,24 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Assertions.Must;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class DraggableElement : MonoBehaviour
+public class DraggableElement : MonoBehaviour, IEndDragHandler, IDragHandler
 {
-
     public GameObject linkedPrefab;
+    Vector3 cursorPosition;
 
-    void OnMouseDrag()
+    public void OnDrag(PointerEventData eventData)
     {
-        Vector3 cursorPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Vector3 translationMouseToObject = cursorPosition - transform.position;
-        transform.position = new Vector3(cursorPosition.x - translationMouseToObject.x, cursorPosition.y - translationMouseToObject.y, -9f);
+        cursorPosition = Camera.main.ScreenToWorldPoint(eventData.position);
+        transform.position = new Vector3(cursorPosition.x, cursorPosition.y, 0f);
+
+        //Fix desync issue
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        // Check if cursor is above UI, if so:
+
+        // Destroy(this.gameObject);
     }
 
     public static void InstantiateFromPrefab(GameObject prefab)
     {
         Vector3 initialInstancePosition = new Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y, 0f);
         GameObject elementInstance = Instantiate(prefab, initialInstancePosition, Quaternion.identity);
+        elementInstance.layer = 8;
 
         foreach (GamePlayElement gamePlayElement in FindObjectsOfType<GamePlayElement>())
         {
@@ -26,6 +36,8 @@ public class DraggableElement : MonoBehaviour
         }
 
         DraggableElement draggableElement = elementInstance.AddComponent<DraggableElement>();
+        draggableElement.linkedPrefab = prefab;
+        
     }
 
 }
--- Project/Assets/Scripts/UI/LevelEditor/LE_ElementButton.cs
+++ Project/Assets/Scripts/UI/LevelEditor/LE_ElementButton.cs
@@ -2,10 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Reflection;
+using UnityEditor;
 
-public class LE_ElementButton : MonoBehaviour
+public class LE_ElementButton : MonoBehaviour, IPointerDownHandler, IEndDragHandler, IDragHandler
 {
     public GameObject prefab;
+    GameObject elementPreview;
 
     void Start()
     {
@@ -14,6 +18,29 @@ public class LE_ElementButton : MonoBehaviour
 
     void OnButtonClick()
     {
+        Destroy(elementPreview);
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        elementPreview = Instantiate(prefab, new Vector3(eventData.position.x, eventData.position.y, -9f), Quaternion.identity);
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Vector3 cursorPosition = Camera.main.ScreenToWorldPoint(eventData.position);
+        elementPreview.transform.position = new Vector3(cursorPosition.x, cursorPosition.y, -9f);
+
+        //Fix desync issues
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Destroy(elementPreview);
+
+        //Check if cursor is above UI. If not:
+
         DraggableElement.InstantiateFromPrefab(prefab);
     }
+
 }
